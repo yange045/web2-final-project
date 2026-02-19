270c6e17db1982eca2ed3a763b7f5f773d64f19a
Eliy V Yang <w3009526@westerntc.edu>
2026-02-19 14:19:46 -0600

    step 2 final project done

--- app.js
+++ app.js
@@ -3,14 +3,49 @@ const port = 8080; // We'll run the server on port 8080
 // IMPORTS
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // MIDDLEWARE
 
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
+// allow the app to get data for form submits
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // ROUTES
 app.get('/', (req, res) => {
-   res.send('<h1>Hello World from Express!</h1>');
+  res.render('home', {
+     title: "My Home Page"
+  });
+});
+
+app.get('/blog', (req, res)=>{
+  res.render('blog-list', {
+    title: "Blog List"
+  });
+});
+
+app.get("/blog/:post", (req, res) => {
+  console.log("The :post param is set to: " +  req.params.post);
+  res.render("blog-post", {
+    title: "Some Title",
+    description: "Some Description",
+    author: "Some Author",
+    published: "Some Date",
+    content: "Some content..."
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+     title: "Contact Me"
+  });
+});
+
+app.post('/contact/submit', (req, res) => {
+  res.send("<h1>TODO: Handle contact form posts</h1>" + JSON.stringify(req.body));
 });
 
 // START THE SERVER
